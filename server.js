2dfd8b0e387dd28b1c5df9141cefbd98a002fde6
Vinicius Bernardo <25652392+vinibern5@users.noreply.github.com>
2018-12-09 23:37:37 -0300

    deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = require('./src/routes');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8000;
 
 // middleware to parse requests body.
 app.use(bodyParser.json({}));
@@ -9,4 +10,4 @@ app.use(bodyParser.json({}));
 // put routes after /probe.
 app.use('/probe', router);
 
-app.listen(3000);
+app.listen(PORT);
